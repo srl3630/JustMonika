5b9fcdd7bb4823fac40a73bf44eb4640b231c3b0
Red_m0rdred <samrlipton@gmail.com>
2018-12-18 18:18:22 -0500

    Update PlayMusic.bat

--- PlayMusic.bat
+++ PlayMusic.bat
@@ -1,1 +1,1 @@
-powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "PlayMusic.ps1 justmonika.wav"
+powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "PlayMusic.ps1" justmonika.wav
